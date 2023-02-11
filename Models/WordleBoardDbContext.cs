f9f22408e69525f36778ecece99e78b40485d847
Ionut Mocanu <ionutmocanu.djk@gmail.com>
2023-02-11 16:24:56 +0200

    Initial commit

---
+++ Models/WordleBoardDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace wordleboard.Models
+{
+    public class WordleBoardDbContext : IdentityDbContext
+    {
+        public WordleBoardDbContext(DbContextOptions<WordleBoardDbContext> options) : base(options)
+        {
+        }
+    }
+}
